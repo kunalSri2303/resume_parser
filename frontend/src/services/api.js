e9effefe46148393165053a9531571b53e43ca66
kunalSri2303 <kunalsrivastav14@gmail.com>
2026-08-11 19:05:55 +0530

    fixed url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'resumeparser-production-9427.up.railway.app';
+const API_BASE_URL = 'https://resumeparser-production-9427.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
